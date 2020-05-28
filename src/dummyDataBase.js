96429060824138ae55565693253a3554c824e655
adrianofelix019 <46236684+adrianofelix019@users.noreply.github.com>
2020-05-28 10:17:05 -0300

    Dummy Database implemented.

--- src/dummyDataBase.js
+++ src/dummyDataBase.js
@@ -2,3 +2,27 @@ const sequence = {
     _id: 1,
     get id() { return this._id++ }
 }
+
+const books = {}
+
+function addBook(book) {
+    if(!book.id) book.id = sequence.id
+    books[book.id] = book
+    return book
+}
+
+function getBook(id) {
+    return books[id] || {}
+}
+
+function getBooks() {
+    return Object.values(books)
+}
+
+function deleteBook(id) {
+    const book = books[id]
+    delete books[id]
+    return book
+}
+
+module.exports = { addBook, getBook, getBooks, deleteBook }
